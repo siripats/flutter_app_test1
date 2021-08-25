280cf3be4568c91745598735961f4e01f159b8a2
siripats <siripats@sau.ac.th>
2021-08-25 17:18:08 +0700

    version 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_test1/views/first_ui.dart';
+import 'package:flutter_app_test1/views/second_ui.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstUI(), // เรียกหน้าจอแรก
+      home: SecondUI(), // เรียกหน้าจอแรก
     ),
   );
 }
--- lib/views/second_ui.dart
+++ lib/views/second_ui.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
 
 class SecondUI extends StatelessWidget {
-  const SecondUI({ Key? key }) : super(key: key);
+  const SecondUI({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
-  }
-class _SecondUIState extends StatelessWidget {
-  const _SecondUIState({ Key? key }) : super(key: key);
+    return Scaffold(
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-      
     );
   }
 }
 
+class _SecondUIState extends State<SecondUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text(
+          'Second UI ^ ^',
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Colors.grey[850],
+            ),
+          ),
+        ],
+      ),
+
+
+      drawer : Drawer(),
+
+      ),
+    );
+  }
 }
